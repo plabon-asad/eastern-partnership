63865666e7f68b128080490cbf3423f7351c7fb9
plabon-asad <plabondotasad@gmail.com>
2020-11-19 18:02:41 +0600

    asset ready and banner design

--- wp-content/themes/eastern-partnership/index.php
+++ wp-content/themes/eastern-partnership/index.php
@@ -5,9 +5,17 @@
 <!--    <title>--><?php //the_title() || 'Eastern Partnership'; ?><!--</title>-->
 
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" >
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
+
+<div class="main-wrap">
+    <section class="top-banner">
+        <div class="container banner-container"></div>
+    </section>
+</div>
 <h1>Lorem Ipsum is simply dummy text</h1>
 <p>
     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
