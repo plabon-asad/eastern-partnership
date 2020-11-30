1a324c6698d7681c40d8817826d34f7424eee30a
plabon-asad <plabondotasad@gmail.com>
2020-11-30 12:00:51 +0600

    custom-js config

--- wp-content/themes/eastern-partnership/assets/js/custom.js
+++ wp-content/themes/eastern-partnership/assets/js/custom.js
@@ -1,6 +1,23 @@
-// console.log('Hi');
-// let bannerContent = $('.banner-content h2');
-// console.log('Hi1', bannerContent)
-// gsap.to( bannerContent, {duration: 1, x: -100, ease: "back.out(1.7)" } )
 
-alert('HI')
+
+$(function (){
+    console.log('Js Nice Working...');
+    let bannerH2 = $('.banner-content h2'),
+        bannerP = $('.banner-content p')
+
+
+
+    gsap.fromTo(
+        [bannerH2, bannerP],
+        {x: -100, opacity: 0.5},
+        {x: 0, opacity: 1, duration: 1, ease: "back.out(1.7)", stagger: 0.2 } )
+
+    // let tl = gsap.timeline();
+    // tl.fromTo( bannerH2, {x: -100}, {x: 0, ease: "back.out(1.7)" } )
+    //     .fromTo(
+    //         bannerP,
+    //         {x: -100, opacity: 0.5},
+    //         {x: 0, opacity: 1, ease: "back.out(1.7)"}
+    //         )
+    // gsap.fromTo( bannerContent, {x: -100}, {x: 0, ease: "back.out(1.7)" } )
+})
